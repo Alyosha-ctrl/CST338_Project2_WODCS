4f6644bb37e6b0dc5bdef5e23962d876dfd1932b
Alyosha-ctrl <alexeibere@gmail.com>
2025-04-19 10:31:18 -0700

    Initial commit, added viewbinding to the build features and altered the login page to fit it.

---
+++ app/src/main/java/com/example/project2_wod_cs/LogIn.java
@@ -0,0 +1,31 @@
+package com.example.project2_wod_cs;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.project2_wod_cs.databinding.ActivityMainBinding;
+
+public class LogIn extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_log_in);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+}
