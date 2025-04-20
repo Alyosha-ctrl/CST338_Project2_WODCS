60cd18c02e1a6b7d4cb56ff4fd3f033446209705
Alyosha-ctrl <alexeibere@gmail.com>
2025-04-20 10:50:34 -0700

    Made Rudimentary launching page, now I need to make it so that pressing the Log In Button goes to the log in screen.

--- app/src/main/java/com/example/project2_wod_cs/MainActivity.java
+++ app/src/main/java/com/example/project2_wod_cs/MainActivity.java
@@ -4,24 +4,42 @@
 package com.example.project2_wod_cs;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.project2_wod_cs.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLogIn();
+            }
         });
+
+        binding.SignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignUp();
+            }
+        });
+    }
+
+    private void startLogIn(){
+        Toast.makeText(MainActivity.this, "Clicked The Log In Button", Toast.LENGTH_SHORT).show();
+    }
+
+    private void startSignUp(){
+        Toast.makeText(MainActivity.this, "Sign Up Not Currently Implemented", Toast.LENGTH_LONG).show();
     }
 }
