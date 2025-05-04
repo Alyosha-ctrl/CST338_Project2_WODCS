61540452d0c9656b13636ff14217714672d8ce31
Alyosha-ctrl <alexeibere@gmail.com>
2025-05-03 22:29:24 -0700

    Initial commit

---
+++ app/src/main/java/com/example/project2_wod_cs/MetaSheets.java
@@ -0,0 +1,40 @@
+package com.example.project2_wod_cs;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.project2_wod_cs.Database.AppDataRepository;
+import com.example.project2_wod_cs.databinding.ActivityMetaSheetsBinding;
+
+public class MetaSheets extends AppCompatActivity {
+
+    ActivityMetaSheetsBinding binding;
+
+    public static final String USERNAME_KEY = "user_id";
+
+    private AppDataRepository repository;
+    private String username = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMetaSheetsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = AppDataRepository.getRepository(getApplication());
+        username = getIntent().getStringExtra(USERNAME_KEY);
+    }
+
+    static Intent metaSheetsIntentFactory(Context context, String username){
+        Intent intent = new Intent(context, LandingPage.class);
+        intent.putExtra(USERNAME_KEY, username);
+        return intent;
+    }
+}
