c07b91dfa875ca703acf709970e694accbfe4c85
Alyosha-ctrl <alexeibere@gmail.com>
2025-05-05 11:10:24 -0700

    Save commit

--- app/src/main/java/com/example/project2_wod_cs/MetaSheets.java
+++ app/src/main/java/com/example/project2_wod_cs/MetaSheets.java
@@ -9,8 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
 
 import com.example.project2_wod_cs.Database.AppDataRepository;
+import com.example.project2_wod_cs.Database.entities.User;
 import com.example.project2_wod_cs.databinding.ActivityMetaSheetsBinding;
 
 public class MetaSheets extends AppCompatActivity {
@@ -18,6 +20,8 @@ public class MetaSheets extends AppCompatActivity {
     ActivityMetaSheetsBinding binding;
 
     public static final String USERNAME_KEY = "user_id";
+
+    private User userWithCharacterSheets;
 
     private AppDataRepository repository;
     private String username = "";
@@ -30,6 +34,13 @@ public class MetaSheets extends AppCompatActivity {
 
         repository = AppDataRepository.getRepository(getApplication());
         username = getIntent().getStringExtra(USERNAME_KEY);
+
+        repository.getUserByUserName(username).observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                user.get
+            }
+        });
     }
 
     static Intent metaSheetsIntentFactory(Context context, String username){
